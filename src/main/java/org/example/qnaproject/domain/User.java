63ba2dae0fb4ade59cfb1f7a097d076a61ae81cb
woogie01 <kwonhu3973@gmail.com>
2024-10-02 15:40:41 +0900

    Feat : User 도메인 추가

---
+++ src/main/java/org/example/qnaproject/domain/User.java
@@ -0,0 +1,33 @@
+package org.example.qnaproject.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    @Column(unique = true)
+    private String username;
+
+    @Builder
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+}
