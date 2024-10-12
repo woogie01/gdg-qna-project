6d89b0a6c54e6a34a76992a1bc36e382de7d6000
woogie01 <kwonhu3973@gmail.com>
2024-10-12 19:06:02 +0900

    Feat : 상수 클래스 추가

---
+++ src/main/java/org/example/qnaproject/util/SessionConst.java
@@ -0,0 +1,6 @@
+package org.example.qnaproject.util;
+
+public class SessionConst {
+    public static final String LOGIN_MEMBER = "loginMember";
+//    public static final String REQUEST_USER_ID = "requestUserId";
+}
