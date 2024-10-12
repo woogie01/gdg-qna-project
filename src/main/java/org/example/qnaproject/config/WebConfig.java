b0e3ac88d0a94a87d2bfdc6f38de45aec3c0d079
woogie01 <kwonhu3973@gmail.com>
2024-10-12 19:08:11 +0900

    Feat : 로그인 관련 인터셉터 설정 추가

---
+++ src/main/java/org/example/qnaproject/config/WebConfig.java
@@ -0,0 +1,27 @@
+package org.example.qnaproject.config;
+
+import org.example.qnaproject.config.argumentresolver.LoginUserArgumentResolver;
+import org.example.qnaproject.config.interceptor.LoginCheckInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(1)
+                .addPathPatterns("/api/**")
+                .excludePathPatterns("/api/user/login", "/api/user/signup");
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new LoginUserArgumentResolver());
+    }
+}
