7f346dd4a1ad7d4af55203b62c939ccfbd6e7818
woogie01 <kwonhu3973@gmail.com>
2024-11-14 20:40:51 +0900

    Feat : Spring Data JPA 형식으로 변경

--- src/main/java/org/example/qnaproject/global/BaseEntity.java
+++ src/main/java/org/example/qnaproject/global/BaseEntity.java
@@ -1,32 +1,23 @@
 package org.example.qnaproject.global;
 
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.PreUpdate;
+import lombok.Data;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
+@Data
+@EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 @Getter
 public class BaseEntity {
-
     @CreatedDate
     private LocalDateTime createDate;
 
     @LastModifiedDate
     private LocalDateTime modifiedDate;
-
-    @PrePersist
-    public void onPrePersist() {
-        this.createDate = LocalDateTime.now();
-        this.modifiedDate = LocalDateTime.now();
-    }
-
-    @PreUpdate
-    public void onPreUpdate() {
-        this.modifiedDate = LocalDateTime.now();
-    }
 }
